9c060bbeb65df735bb7f4c99e92de32fb202f074
treaddevs <s.treadwell11@gmail.com>
2026-06-13 17:34:57 -0400

    Updating header hero, animations, and assets

--- js/role.js
+++ js/role.js
@@ -1,15 +1,5 @@
-const roles = ["UX/UI Designer", "UX Engineer", "Design Systems"];
-const measurer = document.getElementById("role-measurer");
-
-const roleEl = document.querySelector(".dynamic-role");
 const primaryEl = document.querySelector(".primary-role");
-
-const wipeEl = document.querySelector(".wipe");
 const primaryWipeEl = document.querySelector(".primary-wipe");
-
-let index = 2;
-
-roleEl.style.opacity = 0;
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -27,45 +17,27 @@ async function animatePrimaryRoleOnce() {
     void primaryWipeEl.offsetWidth;
 }
 
-async function showNextRole() {
-    let nextIndex = (index + 1) % roles.length;
-    const nextText = `<span class="plus">+</span> ${roles[nextIndex]}`;
+animatePrimaryRoleOnce();
 
-    measurer.innerHTML = nextText;
-    const measuredWidth = measurer.offsetWidth;
-    roleEl.style.width = measuredWidth + "px";
+document.querySelectorAll(".animate-block").forEach((block) =>{
+    const wipe = block.querySelector(".wipe");
+    const text = block.querySelector(".text");
 
-    roleEl.style.opacity = 0;
+    async function animateSubText() {
+        text.style.opacity = 0;
+        await delay(350);
 
-    await delay(350);
+        wipe.style.animation = "wipe-in-out 2s ease forwards";
 
-    wipeEl.style.animation = "wipe-in-out 2s ease forwards";
+        await delay(800);
+        text.style.opacity = 1;
 
-    await delay(800);
-
-    index = nextIndex;
-    roleEl.innerHTML = nextText;
-    roleEl.style.opacity = 1;
-
-    await delay(1150);
-
-    wipeEl.style.animation = "none";
-    void wipeEl.offsetWidth;
-}
-
-async function cycleRoles() {
-    while (true) {
-        await showNextRole();
-        await delay(2000);
+        await delay(1150);
+        wipe.style.animation = "none";
     }
-}
 
-(async function startAnimations() {
-    roleEl.innerHTML = `<span class="plus">+</span> ${roles[index]}`;
-    animatePrimaryRoleOnce();
-    await delay(285);
-    cycleRoles();
-})();
+    animateSubText();
+})
 
 // Intersection Observer for name
 const nameAnimation = new IntersectionObserver(
@@ -93,10 +65,27 @@ const coverImgAnimation = new IntersectionObserver(
             }
         });
     },
+    await delay(500),
     { threshold: 0.5 }
 );
 
 const coverImg = document.querySelector(".cover-img");
 if (coverImg) coverImgAnimation.observe(coverImg);
+
+// Intersection Observer for cta-buttons
+const ctaButtonsAnimation = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                ctaButtonsAnimation.unobserve(entry.target);
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+const ctaButtons = document.querySelector(".cta-buttons");
+if (ctaButtons) coverImgAnimation.observe(ctaButtons);
 
 // console.log(`[${performance.now().toFixed(0)}ms] initial load`);
